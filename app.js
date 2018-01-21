3fb31ba62787c40b92621fc423054c766e5fd96b
DESKTOP-2E0PR3G\Yan <yan@huelsebusch.com>
2018-01-21 18:26:23 +0100

    first init

---
+++ app.js
@@ -0,0 +1,14 @@
+var ammo = require("bitcoin");
+
+var client = new ammo.Client({
+  host: 'localhost',
+  port: 8833,
+  user: 'Ammorpc',
+  pass: 'C7uAfLAwx4nv5qCQ7oHefTxCBfotFMueaAEqXieZad1g',
+  timeout: 30000
+});
+
+client.getBalance('*', 0, (err, balance, resHeaders) => {
+  if (err) return console.log(err);
+  console.log('Balance:', balance);
+})
