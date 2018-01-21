dcd236fe907267a2f315037d5f3e09b0099e06bd
DESKTOP-2E0PR3G\Yan <yan@huelsebusch.com>
2018-01-21 22:27:48 +0100

    express added

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var ammo = require("bitcoin");
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
 
 var client = new ammo.Client({
   host: 'localhost',
@@ -7,6 +14,19 @@ var client = new ammo.Client({
   pass: 'C7uAfLAwx4nv5qCQ7oHefTxCBfotFMueaAEqXieZad1g',
   timeout: 30000
 });
+
+app.post('/getBalance', function (req, res) {
+  var address = req.body.address;
+  if (address) {
+    res.send("Balance for " + address + ": " + "0.150 AMMO");
+  } else {
+    res.send("Address is missing");
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+})
 
 client.getBalance('*', 0, (err, balance, resHeaders) => {
   if (err) return console.log(err);
