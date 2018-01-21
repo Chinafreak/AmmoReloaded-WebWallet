c270d91cd3ca696ba0f64b9527988045dfa8bdd2
DESKTOP-2E0PR3G\Yan <yan@huelsebusch.com>
2018-01-21 22:31:16 +0100

    no message

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ var client = new ammo.Client({
   timeout: 30000
 });
 
-app.post('/getBalance', function (req, res) {
-  var address = req.body.address;
+app.post('/getBalance/:address', function (req, res) {
+  //var address = req.body.address;
+  var address = req.params.address;
   if (address) {
     res.send("Balance for " + address + ": " + "0.150 AMMO");
   } else {
@@ -21,8 +22,8 @@ app.post('/getBalance', function (req, res) {
   }
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(3001, function () {
+  console.log("Example app listening on port 3001!");
 })
 
 client.getBalance('*', 0, (err, balance, resHeaders) => {
