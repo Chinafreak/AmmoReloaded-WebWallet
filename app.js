942cd27a091673ebfced96c3397b80b89bf904f4
DESKTOP-2E0PR3G\Yan <yan@huelsebusch.com>
2018-01-21 22:29:55 +0100

    removed urlencoded

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser')
 app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-  extended: true
-}));
 
 var client = new ammo.Client({
   host: 'localhost',
